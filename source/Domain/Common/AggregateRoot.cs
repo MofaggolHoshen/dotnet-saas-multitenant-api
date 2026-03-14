25c806c05401f1a043a82a478ff7ccbd24a56c92
Mofaggol Hoshen <Mofaggol.Hoshen@outlook.com>
2026-03-14 07:21:21 +0100

    Add abstract AggregateRoot and BaseEntity classes
    Introduced `BaseEntity` with common entity properties and `AggregateRoot` for domain event handling in the `Domain.Common` namespace. Methods for managing domain events are stubbed for future implementation.

---
+++ source/Domain/Common/AggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Common;
+
+public abstract class AggregateRoot : BaseEntity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    protected void AddDomainEvent(IDomainEvent @event) { }
+    public void ClearDomainEvents() { }
+}
